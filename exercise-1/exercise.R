dbae726a81f3d17dd2a3f9c62100f8f1a3724102
Honie Moon <honiemoon@Honies-MacBook-Pro-2.local>
2018-04-05 23:57:32 -0700

    change file to reflect user

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -4,23 +4,24 @@
 
 
 # Assign your name to the variable `my_name`
-
+my_name <- "Honie"
 
 # Assign your height (in inches) to a variable `my_height`
-
+my_height <- 62
 
 # Create a variable `puppies` equal to the number of puppies you'd like to have
-
+puppies <- 2
 
 # Create a variable `puppy_price`, which is how much you think a puppy costs
-
+puppy_price <- 90
 
 # Create a variable `total_cost` that has the total cost of all of your puppies
-
+total_cost <- puppies * puppy_price
 
 # Create a boolean variable `too_expensive`, set to TRUE if the cost is greater 
 # than $1,000
-
+too_expensive <- total_cost > 1000
 
 # Create a variable `max_puppies`, which is the number of puppies you can 
 # afford for $1,000
+max_puppies <- total_cost / 1000
